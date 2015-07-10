5929b13b064e27e276afaeb8d323b48f39be1cce
Rahul Sharma <rahul1419@iiitd.ac.in>
2015-07-10 16:29:50 +0530

    findSuccessor.cpp added

--- findSuccessor.cpp
+++ findSuccessor.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<cstdlib>
 
 using namespace std;
 
@@ -12,8 +13,8 @@ class BST
 		string name;
 		node *parent,*left,*right;
 	}*nptr,*root,*temp;
-	//node* findSuccessor(int);
-	int findLocation(int,node*);		//finds the place in BST where a node is present,or should be present 
+	node* findSuccessor(int);
+	int findLocation(int,node**);		//finds the place in BST where a node is present,or should be present 
 	public:
 	BST(int numNodes)
 	{
@@ -21,11 +22,11 @@ class BST
 		root=nullptr;
 	}
 
-	void createNode(int,string);
-//	void printSuccessor(int);
+	void addNode(int,string);
+	void printSuccessor(int);
 };
 
-void BST::createNode(int a,string s)
+void BST::addNode(int a,string s)
 {
 	nptr=new node;
 	nptr->rnum=a;
@@ -36,7 +37,7 @@ void BST::createNode(int a,string s)
 		return;
 	}
 	
-	int stat=findLocation(a,temp);
+	int stat=findLocation(a,&temp);
 	if (stat==0)		//value not present in the BST
 	{
 		if (temp->rnum<a)
@@ -56,7 +57,7 @@ void BST::createNode(int a,string s)
 	}
 }
 
-int BST::findLocation(int val,BST::node *p)
+int BST::findLocation(int val,BST::node **p)
 {
 	node *prev;
 	prev=temp=root;
@@ -65,7 +66,7 @@ int BST::findLocation(int val,BST::node *p)
 	{
 		if (temp->rnum==val)
 		{
-			p=temp;
+			*p=temp;
 			return 1;
 		}
 
@@ -77,7 +78,7 @@ int BST::findLocation(int val,BST::node *p)
 			}
 			else
 			{
-				p=temp;
+				*p=temp;
 				return 0;
 			}
 		}
@@ -90,14 +91,54 @@ int BST::findLocation(int val,BST::node *p)
 			}
 			else
 			{
-				p=temp;
+				*p=temp;
 				return 0;
 			}
 		}
 	}
 }
 
+BST::node* BST::findSuccessor(int v)
+{
+	int stat=findLocation(v,&nptr);
+	if (stat==0)
+	{
+		cout<<"Node with the given value not present\n";
+		exit(1);
+	}
+	
+	node* prev,*par;
+	prev=nptr;
+	par=nptr->parent;
+	
+	while (prev==par->right)
+	{
+		prev=par;
+		par=par->parent;
+	}
+	
+	return par;
+}
+	
+void BST::printSuccessor(int v)
+{
+	nptr=findSuccessor(v);
+	cout<<"Successor of the node with the value "<<v<<endl;
+	cout<<nptr->rnum<<" "<<nptr->name<<endl;
+}		
+
+
 int main()
 {
+	BST g(8);	
+	g.addNode(134,"Rajnish");	//4
+	g.addNode(130,"Rahul");		//0
+	g.addNode(132,"Rajarshi");	//2
+	g.addNode(131,"Raj");		//1
+	g.addNode(133,"Rajeev");	//3
+	g.addNode(135,"Ravinder");	//5
+	g.addNode(136,"Richa");		//6
+	g.addNode(137,"Resham");	//7
+	g.printSuccessor(133);
 	return 0;
 }
