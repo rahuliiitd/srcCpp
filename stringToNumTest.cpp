f250869823e6ea162c4b75ab42e9bd9776468762
Rahul Sharma <rahul1419@iiitd.ac.in>
2015-07-16 08:27:45 +0530

    stringToNumTest.cpp added

---
+++ stringToNumTest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string i,d;
+	int I;
+	double D;
+	cin>>i>>d;
+	I=stoi(i);
+	D=stod(d);
+	cout<<"\n"<<I<<" "<<D;
+	return 0;
+}
+
